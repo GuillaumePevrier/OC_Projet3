31091e758795e419e18614a0ceea6b41fa905ab8
GuillaumePevrier <guillaumepevrier@gmail.com>
2023-02-01 15:02:50 +0100

    Mise à jour filtres de caatégories et demande aide au mentor

--- FrontEnd/assets/js/home.js
+++ FrontEnd/assets/js/home.js
@@ -6,14 +6,17 @@ const fetchWorks = async () => {
 		.then((promise) => {
 			worksData = promise;
 			console.log(worksData);
+			
 		});
+		
 };
+
 const worksDisplay = async () => {
 	await fetchWorks();
 	
 	document.querySelector('.gallery').innerHTML = worksData.map(
 		(works) => 
-		`<figure id="${works.id}">
+		`<figure id="${works.category.name}" class="displayOn" data-${works.category.name}>
 		 <div id="${works.categoryId}"></div>
 		 <div id="${works.category.name}"></div>
 		 <div id="${works.userId}"></div>
@@ -24,39 +27,30 @@ const worksDisplay = async () => {
 	)
 	.join("");
 };
+
+const worksFilter = async () => {
+	await fetchWorks();
+	
+
+	let filters = document.querySelectorAll("#button_filters_block div");
+	
+	for(let filter of filters){
+		filter.addEventListener("click", function(){
+			let tag = this.id;
+			let figures = document.querySelectorAll("#gallery figure");
+			for(let figure of figures){
+				figure.classList.replace("displayOn", "displayNone");
+				
+				if(tag in figure.dataset || tag === "Tous"){
+					figure.classList.replace("displayNone", "displayOn");
+				}
+			}
+		});
+	}
+
+
+};
+
 worksDisplay();
 
-//Création d'une fonction FILTRE pour filtrer les catégories
-function filterWorks(value) {
-  //Button class code
-  let buttons = document.querySelectorAll(".button_filters");
-  buttons.forEach((button) => {
-	//vérifier si la valeur est égale à innerText
-	if (value.toUpperCase() == button.innerText.toUpperCase()) {
-	  button.classList.add("active");
-	} else {
-	  button.classList.remove("active");
-	}
-  });
-
-  //Sélectionner tous les travaux
-	let elements = document.querySelectorAll(".gallery");
-	//parcourir toutes les travaux
-	elements.forEach((element) => {
-	  //afficher toutes les cartes en cliquant sur le bouton "tous"
-	  if (value == "Tous") {
-		element.classList.remove("hide");
-	  } else {
-		//Vérifier si l'élément contient une class de la catégorie
-		if (element.classList.contains(value)) {
-		  //Afficher les travaux basé sur leur catégorie
-		  element.classList.remove("hide");
-		} else {
-		  //Caché les autres travaux.
-		  element.classList.add("hide");
-		}
-	  }
-	});
-  }
-  
 
